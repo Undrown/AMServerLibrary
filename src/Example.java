b24054b45bda87ade4650450d5f6a7f78f504887
undrown.off@gmail.com <undrown.off@gmail.com>
2020-06-15 09:28:29 +0300

    example with comments

--- src/Example.java
+++ src/Example.java
@@ -1,13 +1,22 @@
 public class Example {
     public static void main(String[] args) {
+        //логинимся
+        //User - это твой Person
+        //В нём такде есть массив со всеми данными
         User user = Connector.performLoginRequest(111L);
+        //загружаем все данные с сервера
         Connector.performPullRequest(user);
-        Entry entryToAdd = new Entry();
-        entryToAdd.timeStart = 100L;
-        entryToAdd.timeStart = 300L;
-        entryToAdd.comment = "autotest";
-        entryToAdd.id = user.id;
-        user.data.add(entryToAdd);
+        //создаём много интервалов (записей)
+        for (int i = 0; i < 100; i++) {
+            Entry entryToAdd = new Entry();
+            entryToAdd.timeStart = 100L+i;
+            entryToAdd.timeStart = 300L+i;
+            entryToAdd.comment = "autotest" + i;
+            entryToAdd.id = user.id;
+            //добавляем к User
+            user.data.add(entryToAdd);
+        }
+        //отправляем всё на сервер
         Connector.performMassivePushRequest(user);
         //System.out.println(user);
     }
